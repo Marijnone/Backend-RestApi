713e98ae544ff13123e05497ef1d2c78927a2126
marijnone <marijn-m@hotmail.com>
2019-05-29 12:25:32 +0000

    -a

--- testdb.js
+++
@@ -1,7 +0,0 @@
-const { MongoClient } = require('mongodb');
-
-MongoClient.connect('mongodb://localhost:27017/mongotest', function(err, db) {
-        console.log('Connected to MongoDB!');
-
-        db.close();
-});
